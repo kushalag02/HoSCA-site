969ead10b7a6568408af4ef8abdfdfe5f24ae05e
raunak-dev-edu <raunak_2101cb47@iitp.ac.in>
2022-08-15 20:22:21 +0530

    raunak work

---
+++ myjs/home.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        const about = $('#wel').height();
+        if($(this).scrollTop()>=(about/2)){
+            $("#about").animate({opacity:'1'});
+
+        }
+    })
+    })
